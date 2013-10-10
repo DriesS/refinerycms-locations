3f55769ed29e6eb31407f141147c00b7295fe952
LuciannaAlmeida <lucianna.th@gmail.com>
2013-10-10 14:13:39 -0300

    fixing some locations specs and removing imports specs

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,6 +1,12 @@
 require 'machinist/active_record'
 require 'faker'
+
+Refinery::Locations::Region.blueprint do
+	name { Faker::Address.city }
+end
+
 Refinery::Locations::Location.blueprint do 
+	region { Refinery::Locations::Region.make! }
 	name { Faker::Company.name }
 	address { Faker::Address.street_address }
 	city { Faker::Address.city }
